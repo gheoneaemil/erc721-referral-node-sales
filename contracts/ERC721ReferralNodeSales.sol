f76b28d505862f0e9255fc6c1f89f0987c7fc1fd
DIIMIIM <emilian@stater.co>
2025-01-06 22:51:58 +0200

    feat: withdraw tokens method now works for everyone, added a canMint flag

--- contracts/ERC721ReferralNodeSales.sol
+++ contracts/ERC721ReferralNodeSales.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/master/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "https://github.com/openzeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/ReentrancyGuard.sol";
 
-contract ERC721ReferralNodeSales is ERC721Enumerable, Ownable, UUPSUpgradeable {
+contract ERC721ReferralNodeSales is ERC721Enumerable, Ownable, UUPSUpgradeable, ReentrancyGuard {
     uint256 public price;
     address public fundsReceiver;
     bool public transferable = true;
+    bool public canMint = true;
     uint256 private _nextTokenId;
 
     struct ReferralCode {
@@ -20,14 +22,14 @@ contract ERC721ReferralNodeSales is ERC721Enumerable, Ownable, UUPSUpgradeable {
     }
 
     mapping(string => ReferralCode) public referralCodes;
-    mapping(address => uint256) public affiliateEarnings;
+    mapping(address => mapping(address => uint256)) public affiliateEarnings;
     mapping(address => bool) public acceptedCurrencies;
 
     event LicenseMinted(address indexed buyer, uint256 tokenId);
     event ReferralCodeCreated(string code, uint256 discountPercentage, uint256 affiliatePercentage, address affiliate);
-    event FundsWithdrawn(address receiver, uint256 amount);
+    event FundsWithdrawn(address receiver, address indexed token, uint256 amount);
 
-    constructor() ERC721("NodeLicense", "NODE") {
+    constructor() Ownable(msg.sender) ERC721("NodeLicense", "NODE") {
         _nextTokenId = 1;
         fundsReceiver = msg.sender;
     }
@@ -37,7 +39,19 @@ contract ERC721ReferralNodeSales is ERC721Enumerable, Ownable, UUPSUpgradeable {
         _;
     }
 
-    function mint(address currency, string memory referralCode) external payable {
+    function getMintPrice(string memory referralCode) external view returns(uint256) {
+        uint256 finalPrice = price;
+        ReferralCode memory ref = referralCodes[referralCode];
+        
+        if (referralCodes[referralCode].active) {    
+            finalPrice -= (price * ref.discountPercentage) / 100;
+        }
+        
+        return finalPrice;
+    }
+
+    function mint(address currency, string memory referralCode) nonReentrant external payable {
+        require(canMint, "Mint is not available yet");
         require(acceptedCurrencies[currency], "Currency not accepted");
         uint256 finalPrice = price;
 
@@ -45,8 +59,8 @@ contract ERC721ReferralNodeSales is ERC721Enumerable, Ownable, UUPSUpgradeable {
             ReferralCode memory ref = referralCodes[referralCode];
             finalPrice -= (price * ref.discountPercentage) / 100;
             if (ref.affiliatePercentage > 0 && ref.affiliate != address(0)) {
-                uint256 affiliateReward = (finalPrice * ref.affiliatePercentage) / 100;
-                affiliateEarnings[ref.affiliate] += affiliateReward;
+                uint256 affiliateReward = (price * ref.affiliatePercentage) / 100;
+                affiliateEarnings[ref.affiliate][currency] += affiliateReward;
             }
         }
 
@@ -65,13 +79,35 @@ contract ERC721ReferralNodeSales is ERC721Enumerable, Ownable, UUPSUpgradeable {
         price = _price;
     }
 
+    function setCanMint(bool _canMint) external onlyOwner {
+        canMint = _canMint;
+    }
+
     function setFundsReceiver(address _receiver) external onlyOwner {
         fundsReceiver = _receiver;
     }
 
-    function withdrawFunds() external onlyOwner {
-        payable(fundsReceiver).transfer(address(this).balance);
-        emit FundsWithdrawn(fundsReceiver, address(this).balance);
+    function withdrawFunds(address token) nonReentrant external {
+        uint256 balance;
+        address receiver = msg.sender == owner() ? fundsReceiver : msg.sender;
+        if (token == address(0)) {
+            balance = receiver == owner() ? address(this).balance : affiliateEarnings[receiver][token];
+            // Withdraw native ETH
+            payable(receiver).transfer(balance);
+            emit FundsWithdrawn(receiver, token, balance);
+        } else {
+            // Withdraw ERC-20 token balance
+            IERC20 tokenContract = IERC20(token);
+            uint256 tokenBalance = tokenContract.balanceOf(address(this));
+            balance = receiver == owner() ? tokenBalance : affiliateEarnings[receiver][token];
+            require(balance > 0, "No token balance to withdraw");
+            tokenContract.transfer(receiver, balance);
+            emit FundsWithdrawn(receiver, token, balance);
+        }
+
+        if (receiver != owner()) {
+            affiliateEarnings[receiver][token] -=balance;
+        }
     }
 
     function toggleTransferability(bool _state) external onlyOwner {
@@ -88,6 +124,7 @@ contract ERC721ReferralNodeSales is ERC721Enumerable, Ownable, UUPSUpgradeable {
         uint256 affiliatePercentage,
         address affiliate
     ) external onlyOwner {
+        require(affiliatePercentage <= discountPercentage, "AP must be <= than DP");
         referralCodes[code] = ReferralCode(discountPercentage, affiliatePercentage, affiliate, true);
         emit ReferralCodeCreated(code, discountPercentage, affiliatePercentage, affiliate);
     }
@@ -106,7 +143,7 @@ contract ERC721ReferralNodeSales is ERC721Enumerable, Ownable, UUPSUpgradeable {
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
-    function _beforeTokenTransfer(
+    function _update(
         address from,
         address to,
         uint256 tokenId,
